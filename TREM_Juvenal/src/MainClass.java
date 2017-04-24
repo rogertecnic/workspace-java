5c2693ff66aa83b4ccd29e85cedd743ef7ce1e2e
pqSEK <pqSEK@172.16.3.6>
2017-04-24 16:23:18 -0300

    testes no TREM_juvenal
    o raio da roda foi calculado, esta perfeito, falta calcular o raio do
    robo e regular o PD, testar com o PID tbm

--- TREM_Juvenal/src/MainClass.java
+++ TREM_Juvenal/src/MainClass.java
@@ -1,3 +1,4 @@
+import lejos.hardware.Button;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
@@ -11,24 +12,25 @@ public class MainClass {
 	EV3LargeRegulatedMotor rodaE;
 	EV3LargeRegulatedMotor rodaD;
 	EV3MediumRegulatedMotor motorG;
-	double acc = 0.5; //m/s^2
-	double velo = 0.8; //m/s
-	double raioRoda = 0.032; //m
-	double larguraRobo = 0.15; //m
-	double P = velo*velo/(2*acc);
+	double acc = 0.3; //m/s^2
+	double velo = 0.2; //m/s
+	double raioRoda = 0.05494/2; //m radio = diametro da roda div por 2
+	double larguraRobo = 0.139; //m largura ta certa
+	double espacoAcc = velo*velo/(2*acc); // espaco que a aceleracao dura
+	double posicaoDesaceleracao = 0;
 
 	int Kp = 1;
 	int Kd = 1;
 	double PD = 0;
 	double erro = 0;
 	double erroAnt = 0;
-	int dt = 10; // tempo do PD em ms
+	int dt = 8; // tempo do PD em ms
 
 
 	public void instance(){
 		rodaE = new EV3LargeRegulatedMotor(MotorPort.A);
 		rodaD = new EV3LargeRegulatedMotor(MotorPort.B);
-		motorG = new EV3MediumRegulatedMotor(MotorPort.C);
+		//motorG = new EV3MediumRegulatedMotor(MotorPort.C);
 		resetMotors();
 	}
 	public void resetMotors(){
@@ -48,6 +50,7 @@ public class MainClass {
 	 * @param graus giro, positivo anti horario, negativo horario
 	 */
 	public void andar(double dist, double graus){
+		instance();
 		double SD = 0;
 		double SE = 0;
 
@@ -57,10 +60,10 @@ public class MainClass {
 
 			graus = graus*3.1415/180;
 			double Sgiro = Math.abs(graus*larguraRobo/2);
-			if(P < Sgiro)
-				P = Sgiro-P;
+			if(espacoAcc < Sgiro)
+				espacoAcc = Sgiro-espacoAcc;
 			else
-				P = Sgiro/2;
+				espacoAcc = Sgiro/2;
 
 			if(graus > 0){ // anti-horario
 				rodaE.backward();
@@ -70,7 +73,7 @@ public class MainClass {
 				rodaE.forward();
 			}
 
-			while(SD<P || SE <P){
+			while(SD<espacoAcc || SE <espacoAcc){
 				Delay.msDelay(dt);
 				SD = Math.abs(rodaD.getTachoCount() - thetaDinicial)*3.1415/180*larguraRobo/2;
 				SE = Math.abs(rodaE.getTachoCount() - thetaEinicial)*3.1415/180*larguraRobo/2;
@@ -80,14 +83,19 @@ public class MainClass {
 				rodaD.setSpeed((float) ((velo - PD)/raioRoda*180/3.1415) );
 				rodaE.setSpeed((float) ((velo + PD)/raioRoda*180/3.1415) );
 			}
+			
 			SD = rodaD.getTachoCount();
 			SE = rodaE.getTachoCount();	
-
-			rodaE.stop(true);
-			rodaD.stop(true);
-			while(SD != rodaD.getTachoCount() || SE != rodaE.getTachoCount()){
+			double SDant = 0;
+			double SEant = 0;
+			while(SD != SDant || SE != SEant){
 				rodaE.stop(true);
 				rodaD.stop(true);
+				Delay.msDelay(30);
+				SDant = SD;
+				SEant = SE;
+				SD = rodaD.getTachoCount();
+				SE = rodaE.getTachoCount();
 			}
 			resetMotors();
 		}
@@ -95,15 +103,15 @@ public class MainClass {
 		{//reta
 			double thetaDinicial = rodaD.getTachoCount();
 			double thetaEinicial = rodaE.getTachoCount();
-			if(P < dist)
-				P = dist-P;
+			if(espacoAcc < dist)
+				espacoAcc = dist-espacoAcc;
 			else
-				P = dist/2;
+				espacoAcc = dist/2;
 
 			rodaE.forward();
 			rodaD.forward();
 
-			while(SD<P || SE<P){
+			while(SD<espacoAcc || SE<espacoAcc){
 				Delay.msDelay(dt);
 				SD = (rodaD.getTachoCount() - thetaDinicial)*3.1415/180*larguraRobo/2;
 				SE = (rodaE.getTachoCount() - thetaEinicial)*3.1415/180*larguraRobo/2;
@@ -115,23 +123,78 @@ public class MainClass {
 			}
 			SD = rodaD.getTachoCount();
 			SE = rodaE.getTachoCount();	
-			rodaE.stop(true);
-			rodaD.stop(true);
-			while(SD != rodaD.getTachoCount() || SE != rodaE.getTachoCount()){
+			double SDant = 0;
+			double SEant = 0;
+			while(SD != SDant || SE != SEant){
 				rodaE.stop(true);
 				rodaD.stop(true);
+				Delay.msDelay(30);
+				SDant = SD;
+				SEant = SE;
+				SD = rodaD.getTachoCount();
+				SE = rodaE.getTachoCount();
 			}
 			resetMotors();
 		}
 	}
 	
+	public void linhaReta(double dist){
+		instance();
+		
+		if(espacoAcc < dist/2)
+			posicaoDesaceleracao = dist-espacoAcc;
+		else
+			posicaoDesaceleracao = dist/2;
+		
+		double SD = 0;
+		double SE = 0;
+		double thetaDinicial = rodaD.getTachoCount();
+		double thetaEinicial = rodaE.getTachoCount();
+		
+
+		rodaE.forward();
+		rodaD.forward();
+
+		while(SD<posicaoDesaceleracao || SE<posicaoDesaceleracao){
+			Delay.msDelay(dt);
+			SD = (rodaD.getTachoCount() - thetaDinicial)*3.1415/180*raioRoda;
+			SE = (rodaE.getTachoCount() - thetaEinicial)*3.1415/180*raioRoda;
+			erroAnt = erro;
+			erro = SD - SE; // erro equivale a diferenca do comprimento dos arcos que rada roda esta fazendo
+			PD = Kp*erro + Kd*(erro - erroAnt)/dt;
+			rodaD.setSpeed((float) ((velo - PD)/raioRoda*180/3.1415) );
+			rodaE.setSpeed((float) ((velo + PD)/raioRoda*180/3.1415) );
+		}
+		
+		SD = rodaD.getTachoCount();
+		SE = rodaE.getTachoCount();
+		double SDant = 0;
+		double SEant = 0;
+		while(SD != SDant || SE != SEant){
+			rodaE.stop(true);
+			rodaD.stop(true);
+			Delay.msDelay(20);
+			SDant = SD;
+			SEant = SE;
+			SD = rodaD.getTachoCount();
+			SE = rodaE.getTachoCount();
+			System.out.println("(" + SE + ", " + SD + ")");
+		}
+		Button.waitForAnyPress();
+		resetMotors();
+		
+	}
 	
-	
-	
+	public void testarRaioDaRoda(){
+		instance();
+		rodaE.rotate((int)(360*5), true);
+		rodaD.rotate((int)(360*5), false);
+	}
 	
 	
 	public static void main(String[] args){
 		MainClass minhaclasse = new MainClass();
-		minhaclasse.andar(1,45);
+		//minhaclasse.testarRaioDaRoda();
+		minhaclasse.linhaReta(1);
 	}
 }
